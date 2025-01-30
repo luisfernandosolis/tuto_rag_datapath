8cb35f9c675a5e3724e842e37f97a0973689e230
Luis Fernando Solís Navarro <lsolis@prediqtdata.com>
2025-01-29 20:45:40 -0500

    changes 2

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
 
-st.write("hola mundo as!")
+with st.sidebar:
+    st.title("RAG WITH STREAMLIT AND GROQ")
+    
+    pdf_file = st.file_uploader("uppload your file", type=["pdf"])
+
+    load_button = st.button(label="let's go!", type="primary")
+    clear_button = st.button(label="clear chat", type="secondary")
+
+
+
